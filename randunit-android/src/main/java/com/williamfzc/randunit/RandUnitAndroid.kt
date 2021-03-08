84d03627093b52cc82248251108627192b23ab5a
williamfzc <178894043@qq.com>
2021-03-08 09:55:34 +0000

    refactor: api split

--- randunit-android/src/main/java/com/williamfzc/randunit/RandUnitAndroid.kt
+++ randunit-android/src/main/java/com/williamfzc/randunit/RandUnitAndroid.kt
@@ -1,9 +1,10 @@
 package com.williamfzc.randunit
 
+import com.williamfzc.randunit.operations.AndroidOperationManager
 import com.williamfzc.randunit.operations.OperationManager
 
-object RandUnit : RandUnitBase() {
+object RandUnitAndroid : RandUnitBase() {
     override fun getOperationManager(): OperationManager {
-        return OperationManager()
+        return AndroidOperationManager()
     }
 }
