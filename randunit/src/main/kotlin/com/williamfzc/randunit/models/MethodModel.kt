28e736606a68903cb2d38fbec62bd973ae4c970d
williamfzc <178894043@qq.com>
2021-03-08 21:36:07 +0800

    fix: statement should be executed inside TestCase

--- randunit/src/main/kotlin/com/williamfzc/randunit/models/MethodModel.kt
+++ randunit/src/main/kotlin/com/williamfzc/randunit/models/MethodModel.kt
@@ -1,11 +1,8 @@
 package com.williamfzc.randunit.models
 
-import com.williamfzc.randunit.extensions.isStatic
 import com.williamfzc.randunit.extensions.isValidType
 import com.williamfzc.randunit.operations.AbstractOperation
-import java.lang.Exception
 import java.lang.reflect.Method
-import java.util.logging.Logger
 
 // desc this method
 class MethodModel(
@@ -14,27 +11,6 @@ class MethodModel(
     private val mockModel: MockModel
 ) {
     private val parametersTypes = method.parameterTypes
-
-    companion object {
-        private val logger = Logger.getLogger("MethodModel")
-    }
-
-    private fun generateCaller(): Any? {
-        // todo: caller only (maybe) be accessed inside test runner
-        // so this method should be called inside TestCase
-        // parameters too!!
-        try {
-            if (method.isStatic())
-                return callerOperation.type
-            return callerOperation.getInstance()
-        } catch (e: Exception) {
-            // failed to create a real caller
-            // use the mock one
-            logger.warning("generate caller $callerOperation failed, use the mock one")
-            e.printStackTrace()
-            return mockModel.mock(callerOperation.type)
-        }
-    }
 
     fun getRelativeTypes(): Set<Class<*>> {
         val typeSet = parametersTypes.toMutableSet()
@@ -45,7 +21,7 @@ class MethodModel(
     }
 
     fun generateStatement(): Statement? {
-        val args = mutableListOf<Any?>()
+        // verify
         for (eachType in parametersTypes) {
 
             // todo: non-null check and attack
@@ -58,13 +34,7 @@ class MethodModel(
             // which returns a `null` always
             if (eachType.canonicalName == "java.lang.Class")
                 return null
-
-            args.add(mockModel.mock(eachType))
         }
-        // gen caller
-        generateCaller()?.let { caller ->
-            return Statement(method, caller, args.toList())
-        }
-        return null
+        return Statement(method, callerOperation, parametersTypes.toList(), mockModel)
     }
 }
--- randunit/src/main/kotlin/com/williamfzc/randunit/models/Statement.kt
+++ randunit/src/main/kotlin/com/williamfzc/randunit/models/Statement.kt
@@ -1,26 +1,70 @@
 package com.williamfzc.randunit.models
 
+import com.williamfzc.randunit.extensions.isStatic
+import com.williamfzc.randunit.operations.AbstractOperation
+import java.lang.Exception
 import java.lang.reflect.Method
 import java.util.logging.Logger
 
 class Statement(
+    // a `method call` may looks like:
+    // methodA(caller, param1, param2)
     private val method: Method,
-    private val caller: Any,
-    private val parameters: List<Any?>
+    private val callerOperation: AbstractOperation,
+    private val parametersTypes: List<Class<*>>,
+
+    // others
+    private val mockModel: MockModel
 ) {
     companion object {
         private val logger = Logger.getLogger("Statement")
     }
 
-    // todo: statement is a data model
-    // should contain types only (and how to get their instances
-    // values will be generated when exec() called
+    // this method should run inside test case
     fun exec() {
-        logger.info("$caller invoking method $method with $parameters")
+        logger.info("[caller ${callerOperation.type}] invoking [method $method] with [params $parametersTypes]")
+
+        val caller = generateCaller()
+        if (null == caller) {
+            logger.warning("generate caller failed, skipped")
+            return
+        }
+
+        val parameters = generateParameters()
+        if (parameters.size != parametersTypes.size) {
+            logger.warning("parameters mock failed, skipped")
+            return
+        }
+
+        // ok, invoke
         method.invoke(caller, *parameters.toTypedArray())
     }
 
     fun getName(): String {
-        return "statement_${caller}_${method}_with_$parameters"
+        return "statement_${method}_with_$parametersTypes"
+    }
+
+    private fun generateCaller(): Any? {
+        try {
+            if (method.isStatic())
+                return callerOperation.type
+            return callerOperation.getInstance()
+        } catch (e: Exception) {
+            // failed to create a real caller
+            // use the mock one
+            logger.warning("generate caller $callerOperation failed, use the mock one")
+            e.printStackTrace()
+            return mockModel.mock(callerOperation.type)
+        }
+    }
+
+    private fun generateParameters(): List<Any?> {
+        val ret = mutableListOf<Any?>()
+        for (each in parametersTypes) {
+            mockModel.mock(each)?.let {
+                ret.add(it)
+            }
+        }
+        return ret
     }
 }
