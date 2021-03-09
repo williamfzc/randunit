9a628c6fb30f8d64f0994e291bea323f7b0cb8e9
williamfzc <178894043@qq.com>
2021-03-09 05:53:44 +0000

    chore: extra desc for env

--- randunit/src/main/kotlin/com/williamfzc/randunit/env/AbstractTestEnv.kt
+++ randunit/src/main/kotlin/com/williamfzc/randunit/env/AbstractTestEnv.kt
@@ -2,6 +2,12 @@ package com.williamfzc.randunit.env
 
 import com.williamfzc.randunit.models.Statement
 
+// todo: current statement == statement model
+// env was designed for running inside something like TestCase/TestSuite
+// which can be used by different runner
+// it manages:
+// - statement model (s)
+// - about how to gen runnable statement from statement model
 abstract class AbstractTestEnv {
     abstract fun run(statement: Statement)
 }
