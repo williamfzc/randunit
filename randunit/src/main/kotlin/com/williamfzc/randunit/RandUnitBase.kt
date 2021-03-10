326cb0406ac415c72deaf2eaac0302494c9bfa4c
williamfzc <178894043@qq.com>
2021-03-10 12:57:09 +0000

    fix: remove `Stream` usage for avoiding android warning

--- randunit/src/main/kotlin/com/williamfzc/randunit/RandUnitBase.kt
+++ randunit/src/main/kotlin/com/williamfzc/randunit/RandUnitBase.kt
@@ -9,10 +9,12 @@ import io.mockk.MockKException
 import org.junit.jupiter.api.DynamicTest
 import java.lang.Exception
 import java.util.logging.Logger
-import java.util.stream.Stream
 
 abstract class RandUnitBase {
-    private val logger = Logger.getLogger("RandUnit")
+    companion object {
+        private val logger = Logger.getLogger("RandUnit")
+    }
+
     abstract fun getOperationManager(): OperationManager
 
     fun collectStatements(
@@ -39,27 +41,23 @@ abstract class RandUnitBase {
     fun runWithTestFactory(
         targetClasses: Set<Class<*>>,
         cfg: ScannerConfig? = null
-    ): Stream<DynamicTest> {
+    ): Iterable<DynamicTest> {
         val dynamicTests: MutableList<DynamicTest> = ArrayList()
 
         collectStatements(targetClasses, cfg).forEach {
-            // each statement will be executed at least twice
-            // make it a config soon
-            for (each in 1..2) {
-                val dynamicTest: DynamicTest = DynamicTest.dynamicTest(it.getName()) {
-                    try {
-                        val env = NormalTestEnv()
-                        env.add(it)
-                        env.start()
-                    } catch (e: Exception) {
-                        if ((e.cause !is MockKException).and(e.cause !is UninitializedPropertyAccessException))
-                            throw e
-                    }
+            val dynamicTest: DynamicTest = DynamicTest.dynamicTest(it.getName()) {
+                try {
+                    val env = NormalTestEnv()
+                    env.add(it)
+                    env.start()
+                } catch (e: Exception) {
+                    if ((e.cause !is MockKException).and(e.cause !is UninitializedPropertyAccessException))
+                        throw e
                 }
-                dynamicTests.add(dynamicTest)
             }
+            dynamicTests.add(dynamicTest)
         }
 
-        return dynamicTests.stream()
+        return dynamicTests
     }
 }
