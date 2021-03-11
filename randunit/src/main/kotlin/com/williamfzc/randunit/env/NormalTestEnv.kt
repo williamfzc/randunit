9746a2a75e0f1f5def77a3611aa9bbe77aff7986
williamfzc <178894043@qq.com>
2021-03-11 15:56:52 +0000

    fix: less exc leak

--- randunit/src/main/kotlin/com/williamfzc/randunit/env/NormalTestEnv.kt
+++ randunit/src/main/kotlin/com/williamfzc/randunit/env/NormalTestEnv.kt
@@ -19,6 +19,7 @@ import com.williamfzc.randunit.exceptions.RUTypeException
 import com.williamfzc.randunit.extensions.isStatic
 import com.williamfzc.randunit.models.StatementModel
 import io.mockk.MockKException
+import org.mockito.exceptions.base.MockitoException
 import java.lang.Exception
 import java.lang.reflect.InvocationTargetException
 import java.lang.reflect.Method
@@ -27,8 +28,20 @@ import java.util.logging.Logger
 class NormalTestEnv(private val envConfig: EnvConfig = EnvConfig()) : AbstractTestEnv(envConfig) {
     companion object {
         private val logger = Logger.getLogger("StandardTestEnv")
-        private val BUILTIN_IGNORED_EXCEPTIONS = setOf(MockKException::class.java)
+        private val BUILTIN_IGNORED_EXCEPTIONS = setOf(
+            MockKException::class.java,
+            MockitoException::class.java,
+            VerifyError::class.java
+        )
+
+        // ignore current exception if these words appeared in traceback msg
+        private val IGNORED_EXCEPTIONS_WORDS = setOf(
+            "org.mockito",
+            ".mockk",
+            "MockitoMock"
+        )
     }
+
     private val ignoredExceptions = BUILTIN_IGNORED_EXCEPTIONS.plus(envConfig.ignoreExceptions)
 
     private fun generateCaller(statementModel: StatementModel): Any? {
@@ -56,38 +69,71 @@ class NormalTestEnv(private val envConfig: EnvConfig = EnvConfig()) : AbstractTe
         return ret
     }
 
+    private fun verifyCallerInst(caller: Any?): Boolean {
+        caller ?: run {
+            logger.warning("generate caller failed, skipped")
+            return false
+        }
+        // base types (kt issues ...
+        if (caller is Unit)
+            return false
+        return true
+    }
+
     override fun run(statementModel: StatementModel) {
         logger.info("running: ${statementModel.getDesc()}")
 
-        val caller = generateCaller(statementModel)
-        caller ?: run {
-            logger.warning("generate caller failed, skipped")
-            return
-        }
+        // at least it will not cause any crashes because of mock
+        runFuncSafely {
+            val caller = generateCaller(statementModel)
+            if (!verifyCallerInst(caller))
+                return@runFuncSafely
+            caller as Any
 
-        val parameters = generateParameters(statementModel)
-        if (parameters.size != statementModel.parametersTypes.size) {
-            logger.warning("parameters mock failed, skipped")
-            return
+            val parameters = generateParameters(statementModel)
+            if (parameters.size != statementModel.parametersTypes.size) {
+                logger.warning("parameters mock failed, skipped")
+                return@runFuncSafely
+            }
+
+            // ok
+            logger.info(
+                """
+                statement initiated:
+                - params: $parameters
+                - caller: $caller
+                - method: ${statementModel.method}
+                """.trimIndent()
+            )
+            return@runFuncSafely runSafely(caller, statementModel.method, parameters)
         }
-        runSafely(caller, statementModel.method, parameters)
     }
 
-    private fun isIgnoredException(e: Exception): Boolean {
+    private fun isIgnoredException(e: Throwable): Boolean {
         for (eachIgnoreException in ignoredExceptions) {
             if (e::class.java == eachIgnoreException)
                 return true
         }
+        // and msg check
+        e.message?.let { errMsg ->
+            for (eachStopWord in IGNORED_EXCEPTIONS_WORDS) {
+                if (errMsg.contains(eachStopWord))
+                    return true
+            }
+        }
+
         return false
     }
 
-    private fun runSafely(caller: Any, method: Method, parameters: List<Any?>) {
-        val returnValueOfInvoke: Any? = try {
-            method.invoke(caller, *parameters.toTypedArray())
+    private fun runFuncSafely(func: () -> Unit): Any? {
+        return try {
+            func()
         } catch (e: Exception) {
+            logger.warning("error happened in runFuncSafely: $e")
             val realException =
                 if ((e is InvocationTargetException).and(null != e.cause))
-                    e.cause as Exception
+                // it can be an Error type
+                    e.cause as Throwable
                 else
                     e
             if (!isIgnoredException(realException))
@@ -96,6 +142,12 @@ class NormalTestEnv(private val envConfig: EnvConfig = EnvConfig()) : AbstractTe
             // else, ignore
             logger.info("error $realException happened but allowed")
             null
+        }
+    }
+
+    private fun runSafely(caller: Any, method: Method, parameters: List<Any?>) {
+        val returnValueOfInvoke: Any? = runFuncSafely {
+            method.invoke(caller, *parameters.toTypedArray())
         }
 
         returnValueOfInvoke?.let { v ->
