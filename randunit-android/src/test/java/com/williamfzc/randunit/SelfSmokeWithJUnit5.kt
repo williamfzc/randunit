31bf1d39bb28b4913f20c907796cecf2fc88e5e8
williamfzc <178894043@qq.com>
2021-03-13 10:43:26 +0800

    test: gson NPE in cases

--- randunit-android/src/test/java/com/williamfzc/randunit/SelfSmokeWithJUnit5.kt
+++ randunit-android/src/test/java/com/williamfzc/randunit/SelfSmokeWithJUnit5.kt
@@ -2,6 +2,7 @@ package com.williamfzc.randunit
 
 import com.williamfzc.randunit.operations.*
 import com.williamfzc.randunit.scanner.Scanner
+import com.williamfzc.randunit.scanner.ScannerConfig
 import org.junit.jupiter.api.DynamicTest
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestFactory
@@ -30,6 +31,13 @@ class SelfSmokeWithJUnit5 {
 
     @TestFactory
     fun scanItself(): Iterable<DynamicTest> {
-        return RandUnitAndroid.runWithTestFactory(clzSet)
+        val scannerConfig = ScannerConfig(
+            includeFilter = setOf("com.williamfzc.randunit"),
+            excludeFilter = setOf("org.jeasy"),
+            excludeMethodFilter = setOf("toJson"),
+            recursively = true,
+            includePrivateMethod = true
+        )
+        return RandUnitAndroid.runWithTestFactory(clzSet, scannerConfig)
     }
 }
