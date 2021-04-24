7d27ede30a49d4387225ee623e0ba873a9f7d9bb
williamfzc <178894043@qq.com>
2021-04-24 16:21:52 +0800

    feat: some rule helper for writing custom rules

--- randunit/src/main/kotlin/com/williamfzc/randunit/env/rules/AbstractRule.kt
+++ randunit/src/main/kotlin/com/williamfzc/randunit/env/rules/AbstractRule.kt
@@ -18,9 +18,52 @@
 package com.williamfzc.randunit.env.rules
 
 import com.williamfzc.randunit.env.Statement
+import java.util.logging.Logger
+
+interface RuleHelper {
+    companion object {
+        private val logger = Logger.getGlobal()
+    }
+
+    // offer some helper functions for doing some judgements
+    // mainly comes from ExceptionExt
+    // which it's hard to use for java users directly
+    private fun err2Trace(e: Throwable): Array<out StackTraceElement>? {
+        e.stackTrace.let {
+            if (it.isNotEmpty())
+                return it
+        }
+        logger.warning("trace is empty: $e")
+        return null
+    }
+
+    fun isHappenedInClass(e: Throwable, className: String): Boolean {
+        err2Trace(e)?.forEach {
+            // its classname is full path format
+            if (it.className.endsWith(className)) {
+                logger.info("class name $className match: happened in $it")
+                return true
+            }
+        }
+        // not match
+        return false
+    }
+
+    fun isHappenedInPackage(e: Throwable, packageName: String): Boolean {
+        err2Trace(e)?.forEach {
+            // its classname is full path format
+            if (it.className.startsWith(packageName)) {
+                logger.info("package $packageName match: happened in $it")
+                return true
+            }
+        }
+        // not match
+        return false
+    }
+}
 
 // rule was designed for judging exceptions
 // return true to mark this err invalid, and it will not be reported
-abstract class AbstractRule {
+abstract class AbstractRule : RuleHelper {
     abstract fun judge(statement: Statement, e: Throwable): Boolean
 }
